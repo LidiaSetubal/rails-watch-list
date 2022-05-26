72a1d7860ee790e402e6552194278de3dfc9c532
Lidia Setubal <lidiasetubalbarbosa@hotmail.com>
2022-05-26 17:05:27 -0300

    router, model, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :lists, only: %i[new index create show] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: %i[destroy]
+
+  root to: 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
